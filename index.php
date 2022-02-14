5d4b24d4012f6f17a48a8cc978dd3fbee8dfe374
sahzad-virasat <sahzad.virasat@gmail.com>
2022-02-14 18:55:03 +0530

    Hello index file

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo 'fdemofdfsdf git repodddddd!';
 
-=======
-echo 'ddemo git repodddddd!';
-echo 'ddemo git repodddddd!';
->>>>>>> parent of 27ee1d4 (Revert "jjjj")
+echo 'fdemofdfsdf git repodddddd!';
+echo 'fdemo git repodddddd!';
+
 ?>
